c96d219bf11a540b7304047931393c3c192da573
zendParadox <raflirama7446@gmail.com>
2025-02-13 14:29:10 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline justify-center items-center h-screen flex">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
