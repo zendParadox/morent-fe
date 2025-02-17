6acce0c1a6aefdd8abba11292df78e9470dcc96c
zendParadox <raflirama7446@gmail.com>
2025-02-17 10:38:38 +0700

    add: card ads

---
+++ src/components/Ads.jsx
@@ -0,0 +1,45 @@
+import CardAds from "./CardAds";
+
+import bg from "../assets/images/bg.png";
+import bg2 from "../assets/images/bg2.png";
+
+function Ads() {
+  return (
+    <>
+      <div className=" mt-12 md:mt-24 grid grid-cols-1 md:grid-cols-2 gap-6 text-white md:gap-8">
+        {/* Kolom Pertama */}
+        <div className="bg-blue-400 rounded-lg">
+          <CardAds
+            title={
+              <>
+                The Best Platform <br /> for Car Rental
+              </>
+            }
+            p="Ease of doing a car rental safely and reliably. Of course at a low
+              price."
+            bg={bg}>
+            asdas
+          </CardAds>
+        </div>
+
+        {/* Kolom Kedua */}
+        <div className="bg-blue-700 rounded-lg">
+          <CardAds
+            title={
+              <>
+                Easy way to rent a <br />
+                cv car at a low price
+              </>
+            }
+            p="Ease of doing a car rental safely and reliably. Of course at a low
+              price."
+            bg={bg2}>
+            asdas
+          </CardAds>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Ads;
---
+++ src/components/CardAds.jsx
@@ -0,0 +1,27 @@
+function CardAds({ title, p, bg, bgButton }) {
+  return (
+    <div className="w-full border h-[300px] md:h-[360px] border-gray-200 rounded-lg shadow relative overflow-hidden">
+      {/* Konten */}
+      <div className="absolute py-4 px-4 md:py-6 md:px-6 z-10">
+        <h3 className="text-2xl md:text-3xl font-semibold">{title}</h3>
+        <p className="mt-2 md:mt-4 text-sm md:text-base">{p}</p>
+        <button
+          type="button"
+          className="px-4 py-2 md:px-5 md:py-3 mt-3 md:mt-5 bg-blue-600 rounded-md text-sm md:text-base">
+          Rental Car
+        </button>
+      </div>
+
+      {/* Gambar sebagai Latar Belakang */}
+      <div className="absolute inset-0 z-0">
+        <img
+          src={bg} // Ganti dengan path gambar Anda
+          alt="Background"
+          className="w-full h-full object-cover"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default CardAds;
