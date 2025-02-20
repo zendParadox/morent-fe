eb14a249a40876e22bd40a2cf395c51aa9b4c191
zendParadox <raflirama7446@gmail.com>
2025-02-20 14:17:05 +0700

    add: page Category

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
+import Category from "./pages/Category";
 
 function App() {
   return (
     <>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/category" element={<Category />} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ src/pages/Category.jsx
@@ -0,0 +1,11 @@
+function Category() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen font-semibold text-xl">
+        Ini category!
+      </div>
+    </>
+  );
+}
+
+export default Category;
